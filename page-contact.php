a0d234abd063fdcf1f12db4fef3595f40e3c6314
Luke Cordova <lukethegreat000@gmail.com>
2025-04-17 17:06:38 -0700

    better contact us page

--- page-contact.php
+++ page-contact.php
@@ -3,11 +3,9 @@
 get_header(); ?>
 
 <main>
-  <h1>Contact Us</h1>
   <?php
   get_template_part('template-parts/contact-section'); ?>
-  
-  <!-- Optional: Embed a form plugin or contact form manually -->
+
 </main>
 
 <?php get_footer(); ?>
