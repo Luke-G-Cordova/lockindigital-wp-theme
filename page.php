fe4995b54557cdb7569de4c00c7c2b3fb69fb8f9
Luke Cordova <lukethegreat000@gmail.com>
2025-04-17 17:33:24 -0700

    better default page.php

--- page.php
+++ page.php
@@ -3,17 +3,21 @@
 /**
  * The template for displaying all pages
  */
-
 get_header(); ?>
 
-<main class="bg-white py-16 text-gray-800">
+<main class="bg-white text-gray-800 py-20">
   <div class="container mx-auto px-6 max-w-4xl" data-sr>
 
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <h1 class="text-4xl font-bold mb-6"><?php the_title(); ?></h1>
-        <div class="prose max-w-none">
-          <?php the_content(); ?>
-        </div>
+        <article class="prose max-w-none">
+          <h1 class="text-4xl font-extrabold mb-6 leading-tight tracking-tight text-gray-900">
+            <?php the_title(); ?>
+          </h1>
+
+          <div class="text-gray-700 leading-relaxed">
+            <?php the_content(); ?>
+          </div>
+        </article>
     <?php endwhile;
     endif; ?>
 
